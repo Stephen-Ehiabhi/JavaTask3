dd62a687097e1574a0c986d8bd7a9ba43ef411f1
Stephen Ehiabhi <Steveyyh123@gmail.com>
2022-03-09 00:41:44 +0100

    fixed main bugs the code works fine and outputs the correct results

---
+++ EquationCalculator.java
@@ -0,0 +1,11 @@
+public class EquationCalculator {
+    public static void main(String[] args){
+
+    }
+
+    //method to solve equation
+    static float quadraticSolution (){
+
+        return ;
+    }
+}
